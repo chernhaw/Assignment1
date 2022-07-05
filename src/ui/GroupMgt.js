b0b3754dac753955c955fa43b7cfb88f778317da
chernhaw <38425578+chernhaw@users.noreply.github.com>
2022-07-05 15:19:13 +0800

    Add files via upload

--- src/ui/GroupMgt.js
+++ src/ui/GroupMgt.js
@@ -14,6 +14,8 @@ function GroupMgt(){
     const [ assigngroup, setAssignGroup] = useState('');
     const [ querygroup, setQueryGroup] = useState('');
     const [ groupmembersresult, setGroupMembersResult] = useState('');
+    const [ unassignmember, setUnassignMember ] = useState('');
+    const [ unassigngroup, setUnassignGroup ] = useState('');
    
     const LogOutUser = () =>{
         alert("You are logged out");
@@ -48,6 +50,66 @@ function GroupMgt(){
         setUsername(event.target.value);
         
     }
+
+    const handleGroupUnAssign= (event) =>{
+        console.log("group unassign "+event.target.value)
+        setUnassignGroup(event.target.value);
+
+    }
+
+    const handleUserNameChangeUnassign= (event) =>{
+        setUnassignMember(event.target.value);
+        
+    }
+
+    const handUpdateGroupUnassign=async(e) =>{
+        e.preventDefault();
+        var groupmembers = "";
+      //  window.localStorage.setItem("groupquery", querygroup);
+        
+        try {
+          
+            alert( "Removing from  group "+ unassigngroup + " username "+ unassignmember)
+            console.log(" "+ unassigngroup +" "+unassignmember)
+
+            const res = await Axios.post('http://localhost:8080/groupremove', 
+            {groupname:""+unassigngroup+"",username:""+unassignmember+""});
+            // {groupname:""+querygroup+""});
+           
+
+          //  {groupname:""+assigngroup+"",username:""+username+""});
+
+           
+            console.log("Query group response "+ res.data);
+            
+           const size = res.data.length;
+           
+        
+           for ( var i=0; i<size; i++){
+             groupmembers = groupmembers+res.data[i].username + " \n"
+            
+           }
+
+           setGroupMembersResult(groupmembers);
+          
+        } catch (e){
+           console.error("Query group error - "+e.message);
+       }
+    }
+    /*
+ const [ unassignmenber, setUnassignMember ] = useState('');
+    const [ unassigngroup, setUnassignGroup ] = useState('');
+
+    */
+
+    // handUpdateGroupUnassign(e)}}>
+    //     <h3>Remove Group Assignment </h3>
+    //         <label>Group Name:</label>
+    //         <input type="text" value={assigngroup} required onChange={(e)=>{handleGroupUnAssign(e)}}/>
+            
+    //         <br/>
+    //         <label>Username :</label>
+    //         <input type="text" value={username} required onChange={(e)=>{handleUserNameChangeUnassign(e)}}/>
     
     const goMain = () =>{
         
@@ -179,10 +241,28 @@ function GroupMgt(){
             <label>Group Name:</label>
             <input type="text" value={assigngroup} required onChange={(e)=>{handleGroupAssign(e)}}/>
             <br/>
-            <br/>
+            
             <label>Username :</label>
             <input type="text" value={username} required onChange={(e)=>{handleUserNameChange(e)}}/>
             <br/>
+            
+            <input type="submit" value="Update Group"/>
+            
+        </form>
+        </div>
+
+
+
+        <div >
+        <form onSubmit={(e)=>{handUpdateGroupUnassign(e)}}>
+        <h3>Remove Group Assignment </h3>
+            <label>Group Name:</label>
+            <input type="text" value={unassigngroup} required onChange={(e)=>{handleGroupUnAssign(e)}}/>
+            
+            <br/>
+            <label>Username :</label>
+            <input type="text" value={unassignmember} required onChange={(e)=>{handleUserNameChangeUnassign(e)}}/>
+            
             <br/>
             <input type="submit" value="Update Group"/>
             
